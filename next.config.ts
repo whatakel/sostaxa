83b075fcb4c842a7a814a6dbdc99f6003feb1244
whatakel <kelvinmlcardoso@gmail.com>
2025-06-27 20:26:04 -0300

    ajustes

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // Requisito para GitHub Pages funcionar com site estático
   output: 'export',
-  basePath: '/sostaxa', // se estiver usando GitHub Pages nesse subdiretório
-  trailingSlash: true,  // para garantir que todas as rotas tenham '/' no final (recomendado com export)
+  basePath: '/sostaxa',
+  trailingSlash: true,
 
   webpack(config) {
     config.cache = {
